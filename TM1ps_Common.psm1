a11801df4d21ee251b13fae91c33057afe2026e8
Ifthen CHERMAK <33223981+ichermak@users.noreply.github.com>
2019-11-03 14:22:17 +0100

    Add files via upload

---
+++ TM1ps_Common.psm1
@@ -0,0 +1,73 @@
+# #################################################################################################################################
+# NAME : TM1ps Module Common
+#
+# DESCRIPTION : This module contains common functions
+#
+# AUTOR : Ifthen CHERMAK
+#
+# LAST UPDATE : 2019/10/31
+#
+# VERSION : V 0.1.1
+#
+# #################################################################################################################################
+
+function Get-IniContent 
+{
+    <#
+        .SYNOPSIS
+        Retrieve the content of an ini file.
+
+        .DESCRIPTION
+        Retrieve the content of an ini file.
+
+        .PARAMETER FilePath
+        The ini file path.
+
+        .INPUTS
+        None. You cannot pipe objects to this function.
+
+        .OUTPUTS
+        System.String. Returns a string ...
+
+        .NOTES
+        None.
+
+        .EXAMPLE
+        None.
+
+        .LINK
+        https://github.com/ichermak/TM1ps
+    #>
+    
+    PARAM 
+    (
+        [Parameter(Mandatory = $true)][STRING]$filePath
+    )
+    
+    $ini = @{ }
+    switch -regex -file $filePath 
+    {
+        "^\[(.+)\]" 
+        { # Section
+            $section = $matches[1]
+            $ini[$section] = @{ }
+            $CommentCount = 0
+        }
+        
+        "^(;.*)$"
+        { # Comment
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = “Comment” + $CommentCount
+            $ini[$section][$name] = $value
+        }
+        
+        "(.+?)\s*=(.*)" 
+        { # Key
+            $name, $value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+    return $ini
+}
+Export-ModuleMember -Function Get-IniContent
